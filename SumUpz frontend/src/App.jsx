2eb61d9a0e7db98f7d083093f44c2037fb136618
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2023-01-14 14:18:42 +0800

    Update Homepage

--- SumUpz frontend/src/App.jsx
+++ SumUpz frontend/src/App.jsx
@@ -4,8 +4,6 @@ import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <div>
@@ -16,12 +14,8 @@ function App() {
       </div>
 
       <Home />
-      <h1> Helllo </h1>
 
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- SumUpz frontend/src/pages/Home.jsx
+++ SumUpz frontend/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 function Home() {
   return (
     <div>
       <h1>Sum Upz</h1>
       <h2> Summarize what you need</h2>
+      <Box
+        sx={{
+          width: 500,
+          maxWidth: "100%",
+        }}
+      >
+        <TextField multiline fullWidth id="fullWidth" />
+      </Box>
     </div>
   );
 }
